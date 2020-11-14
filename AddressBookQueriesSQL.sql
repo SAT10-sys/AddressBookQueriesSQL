59bca350b4c591cd3d59ae63783c3504846bdd52
Satantra Tewari <satantra1508@gmail.com>
2020-11-14 14:41:33 +0530

    [satantra] values inserted into table

--- AddressBookQueriesSQL.sql
+++ AddressBookQueriesSQL.sql
@@ -16,3 +16,16 @@ emailId varchar(50)
 )
 select * from AddressBookTable
 --UC2_TableCreated
+
+insert into AddressBookTable values
+('James','Hetfield','DownTown','San Francisco','California','123456','1111111111','jher@yahoo.com'),
+('Lars','Ulrich','San Jose','San Francisco','California','123456','2222222222','lrt6@yahoo.com'),
+('Kirk','Hammet','DownTown','San Francisco','California','123456','3333333333','khnt5@gmail.com'),
+('Cliff','Burton','Manhattan','New York City','New York','234567','4444444444','cbgt6@gmail.com'),
+('Mike','Shinoda','Queens','New York City','New York','234567','5555555555','msdr45@yahoo.com'),
+('Rajat','Ghosh','Rajarhat','Kolkata','West Bengal','345678','6666666666','rgty6@gmail.com'),
+('Satantra','Tewari','Steel Township','Durgapur','West Bengal','713204','7777777777','iamsatantra@gmail.com'),
+('Sachin','Tendulkar','Bandra','Mumbai','Maharashtra','654321','8888888888','srt200@yahoo.com'),
+('Rohit','Sharma','Andheri','Mumbai','Maharashtra','134567','9999999999','rt264@yahoo.com');
+select * from AddressBookTable
+--UC3_ValuesInsertedInTable

ebb355b907a67c4e8d0baccff4155517ebf84edf
Satantra Tewari <satantra1508@gmail.com>
2020-11-14 14:30:51 +0530

    [satantra] table created

--- AddressBookQueriesSQL.sql
+++ AddressBookQueriesSQL.sql
@@ -3,3 +3,16 @@ select * from sys.databases where name='AddressBookServiceSQL';
 use AddressBookServiceSQL
 --UC1_DatabaseCreated
 
+create table AddressBookTable
+(
+firstName varchar(30),
+lastName varchar(30),
+address varchar(100),
+city varchar(30),
+state varchar(30),
+zipCode varchar(6),
+phoneNumber varchar(10),
+emailId varchar(50)
+)
+select * from AddressBookTable
+--UC2_TableCreated
